a46285d619699962bde62366b1650aa29f3ece3e
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-05-31 23:06:15 +0530

    DB connection done

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express  from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.VITE_MONGO).then(()=>{
+    console.log("DB_Connected")
+}).catch((e)=>{
+    console.log(e);
+})
 
 const app=express();
 
